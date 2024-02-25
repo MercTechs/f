a6e8ae3dd2404f425f284b6c01035d710ca5d665
hglong16 <intihad.vuong@gmail.com>
2024-02-25 22:35:02 +0700

    add: more partial.test

--- src/internal/partial.test.ts
+++ src/internal/partial.test.ts
@@ -4,9 +4,27 @@ function add(a: number, b: number): number {
   return a + b;
 }
 
+function sum(
+  a: number,
+  b: number,
+  c: number
+): number {
+  return a + b + c;
+}
+
 describe("partial", () => {
-  test("partial function with preset arguments", () => {
+  it("partial function with preset arguments", () => {
     const add5 = partial(add, 5);
     expect(add5(10)).toEqual(15);
+  });
+
+  it("partial function with multiple preset arguments", () => {
+    const add5 = partial(sum, 5, 10);
+    expect(add5(15)).toEqual(30);
+  });
+
+  it("partial function with multiple preset arguments", () => {
+    const add5 = partial(sum, 5);
+    expect(add5(10, 15)).toEqual(30);
   });
 });
