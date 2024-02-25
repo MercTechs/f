3b2e8e4b0604ed6edfbdf5d66b29d50154eb54f0
hglong16 <intihad.vuong@gmail.com>
2024-02-25 23:06:00 +0700

    update: coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   test: {
     globals: true,
     coverage: {
-      include: ["src/*.ts"],
+      include: ["src/*.ts", "src/internal/*.ts"],
       exclude: ["src/index.ts", "src/_*.ts"],
     },
   },
