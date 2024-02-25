07396daa9893a346f5b98fed6a4f193fe3db5323
hglong16 <intihad.vuong@gmail.com>
2024-02-25 23:18:11 +0700

    docs: partial + partialRight

--- src/internal/partial.ts
+++ src/internal/partial.ts
@@ -1,3 +1,7 @@
+/**
+ * author: Long <hglong16> Nguyen
+ * create date: 2024-2-25
+ */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 type PartialTuple<
@@ -10,6 +14,17 @@ type PartialTuple<
 type PartialParams<Fn extends (...args: Array<any>) => ReturnType<Fn>> =
   PartialTuple<Parameters<Fn>>;
 
+/**
+ * Creates a partially applied function by fixing some arguments of the original function.
+ * @param fn - The original function to be partially applied.
+ * @param presetArgs - The arguments to be fixed in the partial application.
+ * @returns A new function with the fixed arguments.
+ *
+ * @example
+ * const add = (a: number, b: number) => a + b;
+ * const add5 = partial(add, 5);
+ * const result = add5(10); // 15
+ */
 export function partial<F extends (...args: Array<any>) => ReturnType<F>>(
   fn: F,
   ...presetArgs: PartialParams<F>
@@ -19,6 +34,18 @@ export function partial<F extends (...args: Array<any>) => ReturnType<F>>(
   };
 }
 
+/**
+ * Creates a new function that applies preset arguments to the right side of the original function.
+ * @param fn - The original function.
+ * @param presetArgs - The preset arguments to be applied to the right side of the function.
+ * @returns A new function that applies the preset arguments and invokes the original function.
+ *
+ * @example
+ * const add = (a: string, b: string) => a + b;
+ * const addFirstName = partialRight(add, " Doe");
+ * const result = addFirstName("John"); // "John Doe"
+ *
+ */
 export function partialRight<F extends (...args: Array<any>) => ReturnType<F>>(
   fn: F,
   ...presetArgs: PartialParams<F>
